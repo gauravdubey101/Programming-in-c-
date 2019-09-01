1e715d8ba7eea2a20345c11ac3303f8ee2ad71c0
gaurav Dubey <gauravd638@gmail.com>
2019-09-01 23:26:38 +0530

    classes

---
+++ stl_pair.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<vector>
+#include<string>
+using namespace std;
+	int main(){
+		pair<int,char> p;
+		p = make_pair(2,'b');
+		pair<int,char> p2(1,'a');
+		cout<<p.first<<" "<<p.second<<endl;
+		cout<<p2.first<<" "<<p2.second<<endl;
+	}
