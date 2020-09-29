2978af1baae908abfcfbb2fded25abc4378ec5c3
Gaurav Dubey <gauravd638@gmail.com>
2020-09-30 01:36:18 +0530

    pattern added

---
+++ findreminder.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n;
+	int a;
+	int b;
+	int rem =0;
+	
+	cin >>n;
+	for(int i=0;i<n;i++){
+		cin>>a>>b;
+		 rem = a%b;
+		 	cout<<rem<<endl;
+	}
+	
+}
