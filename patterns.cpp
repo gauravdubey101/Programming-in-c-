620428bbca0a41d298068e9d792d94aba7dab639
Gaurav Dubey <gauravd638@gmail.com>
2020-10-20 23:30:52 +0530

    new session-october 2020

--- patterns.cpp
+++ patterns.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 int main(){
+	int n;
+	cout<<"enter the number"<<endl;
+	cin>>n;
+	
+	for(int i=1;i<=n;i++){
+		for(int s=1;s<=()n-i);s++){
+			cout<<" ";
+		}
+		while()
+	}
 	
 }
