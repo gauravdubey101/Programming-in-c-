ed2bd94c04eacb1c78d9c16bcbeb1b19f8be4875
Gaurav Dubey <gauravd638@gmail.com>
2020-10-23 23:38:09 +0530

    struct and emp -2

--- tut1.cpp
+++ tut1.cpp
@@ -11,24 +11,40 @@ using namespace std;
 //	};
 			
 	typedef struct employee{
-		int eId;
-		char favChar;
-		float salary;
+		int eId;  //4 
+		char favChar; //1
+		float salary; //4
 	}ep;
+		
+//in unioun only one data type can use at a time. 		
+	union money
+	{
+		int rice; //4 inly 4 bytes use for each data type
+		char favcar; //1
+		float pounds; //4
+		};
+		
+		
 		
 	
 	int main(){
 		ep gaurav;
-		struct employee gaurav1;
-		struct employee gaurav2;
-		gaurav.eId=1;
-		gaurav.favChar = 'G';
-		gaurav.salary = 12000000;
+		union money m1;
+		m1.rice=344;
+		cout<<"rice is:"<<m1.rice;
 		
 		
-		cout<<"Slaray is :"<<gaurav.salary<<endl;
-		cout<<"emp id is: "<<gaurav.eId<<endl;
-		cout<<"emp favchar :"<<gaurav.favChar;
+		/* structure*/
+//		struct employee gaurav1;
+//		struct employee gaurav2;
+//		gaurav.eId=1;
+//		gaurav.favChar = 'G';
+//		gaurav.salary = 12000000;
+//		
+//		
+//		cout<<"Slaray is :"<<gaurav.salary<<endl;
+//		cout<<"emp id is: "<<gaurav.eId<<endl;
+//		cout<<"emp favchar :"<<gaurav.favChar;
 		
 		return 0;
 }
